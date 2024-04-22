7d0c78264731bf15c7f8647c2ba9d7be2a4a6eae
danddorr <120369188+danddorr@users.noreply.github.com>
2024-04-22 11:41:08 +0200

    init

---
+++ todo_api/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Todo(models.Model):
+    task = models.CharField(max_length = 180)
+    timestamp = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    completed = models.BooleanField(default = False, blank = True)
+    updated = models.DateTimeField(auto_now = True, blank = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
+
+    def __str__(self):
+        return self.task
+
+"""
+class TodoList(models.Model):
+    name = models.CharField(max_length = 50)
+    timestamp = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    completed = models.BooleanField(default = False, blank = True)
+    updated = models.DateTimeField(auto_now = True, blank = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
+
+    def __str__(self):
+        return self.task
+    
+class Task(models.Model):
+    todolist_id = models.ForeignKey(TodoList, on_delete = models.CASCADE, blank = True, null = True)
+    timestamp = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    completed = models.BooleanField(default = False, blank = True)
+    updated = models.DateTimeField(auto_now = True, blank = True)
+    task = models.CharField(max_length = 300)
+
+    def __str__(self):
+        return self.task
+    
+"""  
